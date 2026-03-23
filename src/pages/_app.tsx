afca4d835f8f1b0f2d48acfecf56e01069c915ce
softgenai <admin@softgen.ai>
2026-03-23 19:19:58 +0000

    fix(theme): Wrap app with ThemeProvider to enable theme context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/contexts/ThemeProvider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </ThemeProvider>
   );
 }
